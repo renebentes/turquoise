7f5f50938f9547f6e460cdfbcdf295b878400545
Rene Bentes Pinto <renebentes@yahoo.com.br>
2014-12-04 16:51:11 -0300

    wip

--- html/mod_cck_list/default.php
+++ html/mod_cck_list/default.php
@@ -10,15 +10,15 @@
 defined('_JEXEC') or die('Restricted access!');
 
 if ($show_list_desc == 1 && $description != '') : ?>
-  <div class="well well-sm"><?php echo JHtml::_( 'content.prepare', $description ); ?></div>
+  <div class="well well-sm"><?php echo JHtml::_('content.prepare', $description); ?></div>
 <?php endif; ?>
 <?php if ($search->content > 0)
   echo $data;
 else
   include dirname(__FILE__) . '/default_items.php';
 if ($show_more_link != '') : ?>
-  <a<?php echo $show_more_class; ?> href="<?php echo $show_more_link; ?>"><?php echo JText::_( 'MOD_CCK_LIST_VIEW_ALL' ); ?></a>
+  <a<?php echo $show_more_class; ?> href="<?php echo $show_more_link; ?>"><?php echo JText::_('MOD_CCK_LIST_VIEW_ALL'); ?></a>
 <?php endif; ?>
 <?php if ($show_list_desc == 2 && $description != '') : ?>
-  <div class="well well-sm"><?php echo JHtml::_( 'content.prepare', $description ); ?></div>
+  <div class="well well-sm"><?php echo JHtml::_('content.prepare', $description); ?></div>
 <?php endif; ?>
